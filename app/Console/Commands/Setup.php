fe409e2b4ed5bee537291942f6170bc4db23c984
Robb Lewis <me@therobb.com>
2016-12-19 09:43:55 +0000

    Better warnings, remove name - we don't need it

--- app/Console/Commands/Setup.php
+++ app/Console/Commands/Setup.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
@@ -43,6 +44,11 @@ class Setup extends Command
     {
     	$this->setupDatabase();
 
+	    if (User::first()) {
+		    $this->warn('User account already created, aborting.');
+		    return;
+	    }
+
 	    if (! $user = $this->createUser()) {
 	    	$this->info('Error: User creation failed. Please try again');
 	    	return;
@@ -53,20 +59,34 @@ class Setup extends Command
 
 	private function setupDatabase()
 	{
-		Artisan::call('migrate:install');
-		Artisan::call('migrate', ['--force' => true]);
+		if (Schema::hasTable('migrations'))
+		{
+			$this->warn('`migrate:install` already run, skipping');
+		} else {
+			$this->info('Running migration install');
+			Artisan::call('migrate:install');
+		}
+
+		if (Schema::hasTable('tweets'))
+		{
+			$this->warn('`migrate` already run, skipping');
+		} else {
+			$this->info('Running table migrations');
+			Artisan::call('migrate', ['--force' => true]);
+		}
 	}
 
 	private function createUser()
 	{
-		$name = $this->ask('Name');
+		$this->info('Creating user account');
+
 		$email = $this->ask('Email Address');
 
 		try
 		{
 			Validator::make(['email' => $email], ['email' => 'email'])->validate();
 		} catch (ValidationException $e) {
-			$this->info('Error: Invalid email. Aborting...');
+			$this->info('Error: Invalid email. aborting.');
 			return false;
 		}
 
@@ -75,12 +95,11 @@ class Setup extends Command
 
 		if ($password != $confirmPassword)
 		{
-			$this->info('Error: Passwords do not match. Aborting...');
+			$this->info('Error: Passwords do not match. aborting.');
 			return false;
 		}
 
 		return User::create([
-			'name' => $name,
 			'email' => $email,
 			'password' => $password
 		]);
